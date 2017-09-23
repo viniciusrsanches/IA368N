88f9420c23aea4ddd7e9cbf540ef04c30bb83344
Vinicius Rodrigues Sanches <viniciusrsanches@gmail.com>
2017-09-23 16:11:41 -0300

    Activity 1 step 3 first commit

--- RoboticLeg/ex03/ex03.m
+++ RoboticLeg/ex03/ex03.m
@@ -15,7 +15,7 @@ J_BF_inB = ...
 
 % what generalized velocity dq do you have to apply in a configuration q = [0;60°;-120°]
 % to lift the foot in vertical direction with v = [0;0;-1m/s];
-v = [0; 0; -1]
+v = [0; 0; -1];
 qi = [0; 60*(pi/180); -120*(pi/180)];
 
 % Determine the numerical value of the foot point jacobian for initial joint angles qi

56f40b548976b992ae26d894ba9a33c13f6ea22a
Vinicius Rodrigues Sanches <viniciusrsanches@gmail.com>
2017-09-26 13:01:52 -0300

    Step 4 revised and matrix symbol replaced by function parameters to speed up the process

--- RoboticLeg/ex04ex05/code/common/vrep/ex04.m
+++ RoboticLeg/ex04ex05/code/common/vrep/ex04.m
@@ -1,7 +1,6 @@
 % Make sure to have the simulation scene mooc_exercise.ttt running in V-REP!
 clear all;
-pkg load symbolic
-syms alpha beta gamma
+
 % simulation setup, will add the matlab paths
 connection = simulation_setup();
 
@@ -26,12 +25,12 @@ simulation_setStepped(connection,true);
 %   J_BF_inB(alpha,beta,gamma) 
 % for the foot positon respectively Jacobian
 
-r_BF_inB = [...
+r_BF_inB = @(alpha, beta, gamma)[...
     -sin(beta + gamma) - sin(beta);...
   sin(alpha)*(cos(beta + gamma) + cos(beta) + 1) + 1;...
   -cos(alpha)*(cos(beta + gamma) + cos(beta) + 1)];
  
-J_BF_inB = [...
+J_BF_inB = @(alpha, beta, gamma)[...
                                               0,             - cos(beta + gamma) - cos(beta),            -cos(beta + gamma);...
  cos(alpha)*(cos(beta + gamma) + cos(beta) + 1), -sin(alpha)*(sin(beta + gamma) + sin(beta)), -sin(beta + gamma)*sin(alpha);...
  sin(alpha)*(cos(beta + gamma) + cos(beta) + 1),  cos(alpha)*(sin(beta + gamma) + sin(beta)),  sin(beta + gamma)*cos(alpha)];
@@ -49,24 +48,23 @@ rGoal = [0.2,0.5,-2]';
 %%% enter here your algorithm
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 qi = q0;
-error_valid = 1
-%step = 0.1
-while error_valid >= 1E-6
-JBF = double(subs(J_BF_inB,[alpha beta gamma],qi'));
-pseudo_inverted_jacobian =  inv(JBF'*JBF)*JBF';
-qGoal = qi + pseudo_inverted_jacobian*(rGoal-double(subs(r_BF_inB,[alpha beta gamma], qi')));
-%qGoal = q0 + inv(J_BF_inB)* (rGoal-double(subs(r_BF_inB,[alpha beta gamma], q0')));
+error_valid = 1;
 
-updatePos(vrep,connection.clientID,qGoal)
-%pause(1/step)
+while error_valid >= 1E-8
+JBF = J_BF_inB(qi(1),qi(2),qi(3));
+pseudo_inverted_jacobian =  inv(JBF'*JBF)*JBF';
+qGoal = qi + pseudo_inverted_jacobian*(rGoal-r_BF_inB(qi(1),qi(2),qi(3)));
 qi = qGoal;
 valid
 endwhile
+updatePos(vrep,connection.clientID,qGoal)
+
+
 % now disable stepped simulation mode:
 simulation_setStepped(connection,false);
 
 
-pause(0.5)
+pause(5)
 
 % stop the simulation
 simulation_stop(connection);

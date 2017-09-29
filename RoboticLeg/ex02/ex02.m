49c3ae4fdf7047b774c02a3bf1af8e57c0de17d8
Vinicius Rodrigues Sanches <viniciusrsanches@gmail.com>
2017-09-29 19:29:26 -0300

    carol solution adaptation

--- RoboticLeg/ex02/ex02.m
+++ RoboticLeg/ex02/ex02.m
@@ -5,15 +5,15 @@ syms alpha beta gamma real
 lb = 1; l1 = 1; l2 = 1; l3 = 1;
 
 % rotational matrices calculated in previous problem set
-R_B1 = [1 0 0;0 cos(alpha) -sin(alpha);0 sin(alpha) cos(alpha)];
-R_12 = [cos(beta) 0 sin(beta);0 1 0;-sin(beta) 0 cos(beta)];
-R_23 = [cos(gamma) 0 sin(gamma);0 1 0;-sin(gamma) 0 cos(gamma)];
+R_B1 = [[1 0 0];[0 cos(alpha) -sin(alpha)];[0 sin(alpha) cos(alpha)]];
+R_12 = [[cos(beta) 0 sin(beta)];[0 1 0];[-sin(beta) 0 cos(beta)]];
+R_23 = [[cos(gamma) 0 sin(gamma)];[0 1 0];[-sin(gamma) 0 cos(gamma)]];
 
 % write down the 3x1 relative position vectors for link length l_i=1
-r_B1_inB = [0,0,-1]';
-r_12_in1 = [0,0,-1]';
-r_23_in2 = [0,0,-1]';
-r_3F_in3 = [0,1,0]';
+r_B1_inB = [0,lb,0]';
+r_12_in1 = [0,0,-l1]';
+r_23_in2 = [0,0,-l2]';
+r_3F_in3 = [0,0,-l3]';
 
 % write down the homogeneous transformation matrices
 H_B1 = [R_23,r_23_in2;0 0 0 1];

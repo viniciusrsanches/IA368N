105d58014b594e91e7c1ba14c54b1cc36e34e4f1
Vinicius Rodrigues Sanches <viniciusrsanches@gmail.com>
2017-11-23 03:03:46 -0200

    Activity 4 exercise 1 solution

--- Atividade4/IA368 ex4/code/common/vrep/transitionFunction.m
+++ Atividade4/IA368 ex4/code/common/vrep/transitionFunction.m
@@ -8,9 +8,33 @@ function [f, F_x, F_u] = transitionFunction(x,u, l)
 % State and input are defined according to "Introduction to Autonomous Mobile Robots", pp. 337
 
 %STARTRM
-f = #;
+pkg load symbolic
+%
+syms Delta_r Delta_l th X Y b
+%b = l/2;
+%Delta_r = u(2);
+%Delta_l = u(1);
+%Delta_S = ((Delta_r + Delta_l)/2);
+%Delta_th= ((Delta_r - Delta_l)/l);
 
-F_x = #;
+%X = x(1);
+%Y = x(2);
+%th = x(3);
 
-F_u = #;
+f = [X Y th]'+[ (((Delta_r + Delta_l)/2)*cos(th+(((Delta_r - Delta_l)/(2*b))/2))); ...
+                (((Delta_r + Delta_l)/2)*sin(th+(((Delta_r - Delta_l)/(2*b))/2))); ... 
+                ((Delta_r - Delta_l)/(2*b))];
+
+
+                
+%Q_F_x = jacobian(f,[X Y th]);                
+F_x = [ 1 0 -(Delta_S*sin(th+(Delta_th/2))); ...
+        0 1 (Delta_S*cos(th+(Delta_th/2))); ...
+        0 0 1];
+
+V1 = cos(th+(Delta_th/2))/2 + (Delta_S/(2*l))*sin(th+(Delta_th/2));
+V2 = cos(th+(Delta_th/2))/2 - (Delta_S/(2*l))*sin(th+(Delta_th/2));
+V3 = sin(th+(Delta_th/2))/2 - (Delta_S/(2*l))*cos(th+(Delta_th/2));
+V4 = sin(th+(Delta_th/2))/2 + (Delta_S/(2*l))*cos(th+(Delta_th/2));
+F_u = [V1 V2; V3 V4; (-1/(l)) (1/(l))] ;
 %ENDRM

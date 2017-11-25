9057a5ac328ff01e7a61208cf6f27c1ca154d7e3
Vinicius Rodrigues Sanches <viniciusrsanches@gmail.com>
2017-11-25 21:32:51 -0200

    Partial solution activity 4

--- Atividade4/IA368 ex4/code/common/vrep/filterStep.m
+++ Atividade4/IA368 ex4/code/common/vrep/filterStep.m
@@ -4,11 +4,14 @@ function [x_posteriori, P_posteriori] = filterStep(x, P, u, Z, R, M, k, g, l)
 
 %STARTRM
 
+ 
+Delta_l = u(1);
+Delta_r = u(2);
 % propagate the state (p. 337) , here kr=kl=k
-Q = #; 
+Q = [k*Delta_l 0; 0 k*Delta_r]; 
 
-[x_priori, F_x, F_u] = #; %hints: you just coded this function
-P_priori = #;
+[x_priori, F_x, F_u] = transitionFunction(x,u,l);
+P_priori = F_x*P*F_x' + F_u*Q*F_u';
 
 if size(Z,2) == 0
     x_posteriori = x_priori;
@@ -16,15 +19,15 @@ if size(Z,2) == 0
     return;
 end
     
-[v, H, R] = #;%hints: you just coded this function
+[v, H, R] = associateMeasurements(x, P, Z, R, M, g);
 
 y = reshape(v, [], 1);
 H = reshape(permute(H, [1,3,2]), [], 3);
 R = blockDiagonal(R);
 
 % update state estimates (pp. 335)
-S = #; %S is the innovation covariance matrix
-K = #;
+S = H*P_priori*H'+R;
+K = P_priori*H'*inv(S);
 
 x_posteriori = x_priori + K * y;
 P_posteriori = (eye(size(P_priori)) - K*H) * P_priori;
